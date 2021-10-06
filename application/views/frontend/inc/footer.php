9da3baf05169ffe0767965429cff6272a6383487
gagan <gagancool56@gmail.com>
2021-10-07 01:54:16 +0530

    Header and footer made dynamic

---
+++ application/views/frontend/inc/footer.php
@@ -0,0 +1,38 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-lg-6 text-lg-left text-center">
+                <div class="copyright">
+                    &copy; Copyright <strong>Shiptree</strong>. All Rights Reserved
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
+                    <a href="#intro" class="scrollto">Home</a>
+                    <a href="#about" class="scrollto">About</a>
+                    <a href="#">Privacy Policy</a>
+                    <a href="#">Terms of Use</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="<?= base_url('assets/frontend/vendor/aos/aos.js') ?>"></script>
+<script src="<?= base_url('assets/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('assets/frontend/vendor/glightbox/js/glightbox.min.js') ?>"></script>
+<script src="<?= base_url('assets/frontend/vendor/isotope-layout/isotope.pkgd.min.js') ?>"></script>
+<script src="<?= base_url('assets/frontend/vendor/php-email-form/validate.js') ?>"></script>
+<script src="<?= base_url('assets/frontend/vendor/purecounter/purecounter.js') ?>"></script>
+<script src="<?= base_url('assets/frontend/vendor/swiper/swiper-bundle.min.js') ?>"></script>
+
+<!-- Template Main JS File -->
+<script src="<?= base_url('assets/frontend/js/main.js') ?>"></script>
+
+</body>
+
+</html>
